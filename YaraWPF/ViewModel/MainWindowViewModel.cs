3ae45cf8816a8b1ae1bd521dc4ded96bcd7c5491
Jarred Capellman <jcapellman@hotmail.com>
2019-02-03 18:57:51 -0600

    Added: Initial commit

---
+++ YaraWPF/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using YaraSharp;
+
+namespace YaraWPF.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private YSInstance ysInstance = new YSInstance();
+
+        public MainWindowViewModel()
+        {
+
+        }
+
+        private void InitializeYara()
+        {
+            using (YSContext context = new YSContext())
+            {
+                //	Compiling rules
+                using (YSCompiler compiler = ysInstance.CompileFromFiles(ruleFilenames, externals))
+                {
+                    //  Get compiled rules
+                    YSRules rules = compiler.GetRules();
+
+                    //  Get errors
+                    YSReport errors = compiler.GetErrors();
+                    //  Get warnings
+                    YSReport warnings = compiler.GetWarnings();
+
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
